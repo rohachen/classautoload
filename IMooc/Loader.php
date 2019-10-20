4d000aba2ead34ec50c3cbbd65ca7f1f6adaad75
Rohan <707761430@qq.com>
2019-10-20 09:59:24 +0800

    init oop frame

---
+++ IMooc/Loader.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace IMooc;
+
+/**
+ * 
+ */
+class Loader
+{
+	
+	static function autoload($class)
+	{
+		require BASEDIR.'/'.str_replace('\\','/',$class).'.php';
+
+	}
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+// psr-0
+// 命名空间与路径一致
+// 文件名与类名一致
+// 类中不可有可执行代码
+// 类首字母大写
+
+define('BASEDIR',__DIR__);
+
+
+include(BASEDIR.'\IMooc\Loader.php');
+
+spl_autoload_register('\\IMooc\\Loader::autoload');
+
+
+IMooc\Object::test();
